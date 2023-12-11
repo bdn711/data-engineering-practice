1a89e7583c520059dd118bad531934f7828b0bfb
Brett Noble <bdn711@gmail.com>
2023-12-10 23:30:32 -0800

    Add additional code for installing bs4 as a workaround for error when running docker-compose up run

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,6 +1,17 @@
 import requests
 import pandas
-from bs4 import BeautifulSoup
+import subprocess
+import sys
+
+# additional code due to bs4 behaving unexpectedly when working with subprocess that interfered with docker
+def install_bs4():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "bs4"])
+
+try:
+    from bs4 import BeautifulSoup
+except:
+    install_bs4()
+    from bs4 import BeautifulSoup
 
 
 def main():
