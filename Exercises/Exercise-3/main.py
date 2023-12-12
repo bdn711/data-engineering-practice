a0685a5c90dc1cf6da3736423f152a883a0f377c
Brett Noble <bdn711@gmail.com>
2023-12-11 20:23:29 -0800

    Add complete Exercise 3 code

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,8 +1,36 @@
 import boto3
+import gzip
 
 
 def main():
-    # your code here
+
+    # boto3 download the file from s3 located at bucket commoncrawl and key 
+    # crawl-data/CC-MAIN-2022-05/wet.paths.gz
+    s3 = boto3.client('s3', region_name='us-east-1')
+    s3.download_file(
+        'commoncrawl',
+        'crawl-data/CC-MAIN-2022-05/wet.paths.gz',
+        'wet.paths.gz'
+        )
+
+
+    # Extract and open this file with Python and pull uri from the first line
+    with gzip.open('wet.paths.gz', 'rt') as file:
+        uri = file.readline().strip()
+
+    # Download the uri file (from first file line) from s3 using boto3
+    filename = uri.split('/')[-1]
+    s3.download_file(
+        'commoncrawl',
+        uri,
+        filename
+        )
+
+    # Print each line to terminal
+    with gzip.open('wet.paths.gz', 'rt') as file:
+        for line in file:
+            print(line)
+
     pass
 
 
